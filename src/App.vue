72b3fd097697c6c8acdfb2db8102c22814378657
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-15 16:45:04 +0200

    feat: MainBanner

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
     <MainHeader/>
+    <MainBanner/>
+    <MainProducts/>
+    <MainFooterInfo/>
+    <MainFooterLinks/>
   </div>
 </template>
 
 <script>
 import MainHeader from './components/MainHeader.vue'
+import MainBanner from './components/MainBanner.vue'
+import MainProducts from './components/MainProducts.vue'
+import MainFooterInfo from './components/MainFooterInfo.vue'
+import MainFooterLinks from './components/MainFooterLinks.vue'
+
 
 export default {
   name: 'App',
   components: {
     MainHeader,
+    MainBanner,
+    MainProducts,
+    MainFooterInfo,
+    MainFooterLinks
   }
 }
 </script>
---
+++ src/components/MainBanner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ms_container mt-4">
+      <div class="container d-flex align-items-center">
+        <h1>Content goes here</h1>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variabiles.scss";
+
+div .ms_container{
+    background-color: $standardBg;
+    
+
+    .container{
+        height: 7rem;
+    }
+
+    h1{
+        color: $textColor;
+    }
+}
+
+</style>
