5635259c94cd9379a0ac0ae1ee730201b157d330
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-15 15:22:56 +0200

    feat: header with nav menu and logo

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <MainHeader/>
+  </div>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+@import url('~boostrap/scss/boostrap.scss');
+
+}
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,85 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <img src="../assets/img/dc-logo.png" alt="dc logo">
+        </div>
+        <div class="col">
+            <ul>
+                <li v-for="(items, index) in navList" :key="index">{{items.text}}</li>
+            </ul>
+
+        </div>
+
+    </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            navList: [
+                {
+                    text: "CHARACTERS",
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: "COMICS",
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: "MOVIES",
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: "TV",
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: "GAMES",
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: "VIDEOS",
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: "FANS",
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: "NEWS",
+                    url: '#',
+                    active: false
+                },
+                {
+                    text: "SHOP",
+                    url: '#',
+                    active: false
+                },
+
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+
+</style>
